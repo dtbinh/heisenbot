72fc3bcada84a6074ccba0e728f621267940d590
klanec <nicholas.ss.harris@gmail.com>
2014-02-10 21:35:08 +0000

    Update LineFollow.java

--- Assignment 2/LineFollow.java
+++ Assignment 2/LineFollow.java
@@ -11,40 +11,60 @@ public class LineFollow extends RobotConfig {
 	
 	static RegulatedMotor l_motor = Motor.B;
     static RegulatedMotor r_motor = Motor.C;
-    static DifferentialPilot heisenbot = new DifferentialPilot(56, 165, l_motor, r_motor);
+    static DifferentialPilot heisenbot = new DifferentialPilot(56, 153, l_motor, r_motor);
 	static LightSensor l_eye = new LightSensor(SensorPort.S1);
 	static LightSensor r_eye = new LightSensor(SensorPort.S2);
-	static int arcVal = 20;
-	static int arcAng = 20;
 	
 	public static void main(String[] args) {
 		
 		Button.waitForAnyPress();
-		heisenbot.setTravelSpeed(500);
 		Delay.msDelay(500);
 		
+		heisenbot.setTravelSpeed(100);
+		heisenbot.setRotateSpeed(100);
+		
 		while(Button.ENTER.isUp()) {
-			if(straightCondition()) { //line between eyes
-				heisenbot.setTravelSpeed(500);
-				heisenbot.travel(20, true);
-			} else if(bothCondition()) { //line under both eyes
-				heisenbot.quickStop();
-				System.out.println("it works");
-				Button.waitForAnyPress();
-				Delay.msDelay(500);
-				heisenbot.travel(20);
-			} else if(leftCondition()) { //line under left eye
-				heisenbot.setTravelSpeed(300);
-				System.out.println("LEFT");
-				heisenbot.arc(arcVal, arcAng, true);
-			} else if(rightCondition()) { //line under right eye
-				heisenbot.setTravelSpeed(300);
-				System.out.println("RIGHT");
-				heisenbot.arc(-arcVal, -arcAng, true);
-			} else {
-				heisenbot.quickStop();
-				System.out.println("something went wrong");
+			
+			int l = l_eye.readValue();
+			int r = r_eye.readValue();
+			
+			if(l >= l_turnCondition && r >= r_turnCondition) { //line between eyes
+				if(juncCheck()) {
+					both();
+					route++;
+				} else {
+					straight();
+				}
+			} 
+			
+			if(l_eye.readValue() < l_turnCondition && r_eye.readValue() < r_turnCondition) { //line under both eyes 
+				both();
+				route++;
+			} 
+			
+			if(l <= l_turnCondition && r >= r_turnCondition) { //line under left eye
+				heisenbot.travel(10);
+				if(juncCheck()) {
+					both();
+					route++;
+				} else {
+					left();
+				}
+				System.out.println("turn successful");
+			} 
+			
+			if(l >= l_turnCondition && r <= r_turnCondition) { //line under right eye
+				heisenbot.travel(10);
+				if(juncCheck()) {
+					both();
+					route++;
+				} else {
+					right();
+				}
+				System.out.println("turn successful");
 			}
 		}
 	}
 }
+
+
