1bd227bb271234dd5dbbea66f4dda0133f3bdf42
klanec <nicholas.ss.harris@gmail.com>
2014-02-10 15:31:51 +0000

    Update LineFollow.java

--- Assignment 2/LineFollow.java
+++ Assignment 2/LineFollow.java
@@ -14,32 +14,37 @@ public class LineFollow extends RobotConfig {
     static DifferentialPilot heisenbot = new DifferentialPilot(56, 165, l_motor, r_motor);
 	static LightSensor l_eye = new LightSensor(SensorPort.S1);
 	static LightSensor r_eye = new LightSensor(SensorPort.S2);
+	static int arcVal = 20;
+	static int arcAng = 20;
 	
 	public static void main(String[] args) {
 		
 		Button.waitForAnyPress();
-		heisenbot.setTravelSpeed(800);
+		heisenbot.setTravelSpeed(500);
 		Delay.msDelay(500);
 		
 		while(Button.ENTER.isUp()) {
 			if(straightCondition()) { //line between eyes
-				heisenbot.travel(10, true);
+				heisenbot.setTravelSpeed(500);
+				heisenbot.travel(20, true);
+			} else if(bothCondition()) { //line under both eyes
+				heisenbot.quickStop();
+				System.out.println("it works");
+				Button.waitForAnyPress();
+				Delay.msDelay(500);
+				heisenbot.travel(20);
 			} else if(leftCondition()) { //line under left eye
-				heisenbot.arcForward(20);
-				if(bothCondition()) {
-					solveHairpinLeft();
-				}
+				heisenbot.setTravelSpeed(300);
+				System.out.println("LEFT");
+				heisenbot.arc(arcVal, arcAng, true);
 			} else if(rightCondition()) { //line under right eye
-				heisenbot.arcForward(-20);
-				if(bothCondition()) {
-					solveHairpinRight();
-				}
-			} //else if(bothCondition()) { //line under both eyes
-				//heisenbot.quickStop();
-				//explore();
-			//}
+				heisenbot.setTravelSpeed(300);
+				System.out.println("RIGHT");
+				heisenbot.arc(-arcVal, -arcAng, true);
+			} else {
+				heisenbot.quickStop();
+				System.out.println("something went wrong");
+			}
 		}
-
 	}
-
 }
