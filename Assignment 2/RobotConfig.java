54ed8c6053bccfc7a5f3d6d047ce23a5ebce6ba1
Aezure <retep.ster@yahoo.co.uk>
2014-02-11 13:19:29 +0000

    Update RobotConfig.java
    comments.

--- Assignment 2/RobotConfig.java
+++ Assignment 2/RobotConfig.java
@@ -7,8 +7,8 @@ import lejos.robotics.navigation.DifferentialPilot;
 public class RobotConfig {
 
 	static RegulatedMotor l_motor = Motor.B;
-    static RegulatedMotor r_motor = Motor.C;
-    static DifferentialPilot heisenbot = new DifferentialPilot(56, 165, l_motor, r_motor);
+    	static RegulatedMotor r_motor = Motor.C;
+    	static DifferentialPilot heisenbot = new DifferentialPilot(56, 165, l_motor, r_motor);
 	static LightSensor l_eye = new LightSensor(SensorPort.S1);
 	static LightSensor r_eye = new LightSensor(SensorPort.S2);
 	static int l_turnCondition = 36;
@@ -17,19 +17,24 @@ public class RobotConfig {
 	static int arcAng = 20;
 	static int route = 0;
 	
+	//he moves forward...
 	public static void straight() {
 		heisenbot.travel(10, true);
 	}
-	
+
+	//goes straight over junction...
 	public static void juncStraight() {
 		heisenbot.travel(20);
 		straight();
 	}
+	
+	//turns round...
 	public static void reverse() {
 		heisenbot.rotate(180);
 		straight();
 	}
-	
+
+	//decides on action at a junction...
 	public static void both() {
 		heisenbot.quickStop();
 		System.out.println("junction");
@@ -63,30 +68,35 @@ public class RobotConfig {
 			juncRight();
 			route = 0;
 		}
-		
+
 		System.out.println("Junction successful");
 	}
-	
+
+	//curves to the left
 	public static void left() {
 		System.out.println("LEFT");
 		heisenbot.arc(arcRad, arcAng, false);
 	}
 	
+	//turns left
 	public static void juncLeft() {
 		heisenbot.travel(75);
 		heisenbot.rotate(90);
 	}
-	
+
+	//curves to the right
 	public static void right() {
 		System.out.println("RIGHT");
 		heisenbot.arc(-arcRad, -arcAng, false);
 	}
-	
+
+	//turns right
 	public static void juncRight() {
 		heisenbot.travel(75);
 		heisenbot.rotate(-90);
 	}
-	
+
+	//checks for a junction
 	public static boolean juncCheck() {
 		boolean run = false;
 		if(l_eye.readValue() < l_turnCondition && r_eye.readValue() < r_turnCondition) {
@@ -94,19 +104,18 @@ public class RobotConfig {
 		}
 		return run;
 	}
-	
+
+	//unused
 	public static void solveHairpinLeft() {
 		System.out.println("HAIRPIN LEFT");
 		heisenbot.travel(60);
 		heisenbot.rotate(70);
 	}					
 	
+	//unused
 	public static void solveHairpinRight() {
 		System.out.println("HAIRPIN RIGHT");
 		heisenbot.travel(60);
 		heisenbot.rotate(-70);
 	}
-	 
-	
 }
-
