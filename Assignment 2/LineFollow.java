b0856101b42cb61cabaa73735b1bd74c0a0f1bd9
Aezure <retep.ster@yahoo.co.uk>
2014-02-11 13:20:31 +0000

    Update LineFollow.java
    commented out unneeded stuff

--- Assignment 2/LineFollow.java
+++ Assignment 2/LineFollow.java
@@ -1,33 +1,33 @@
 import lejos.nxt.Button;
-import lejos.nxt.LightSensor;
-import lejos.nxt.Motor;
-import lejos.nxt.SensorPort;
-import lejos.robotics.RegulatedMotor;
-import lejos.robotics.navigation.DifferentialPilot;
+//import lejos.nxt.LightSensor;
+//import lejos.nxt.Motor;
+//import lejos.nxt.SensorPort;
+//import lejos.robotics.RegulatedMotor;
+//import lejos.robotics.navigation.DifferentialPilot;
 import lejos.util.Delay;
 
 
 public class LineFollow extends RobotConfig {
-	
-	static RegulatedMotor l_motor = Motor.B;
-    static RegulatedMotor r_motor = Motor.C;
-    static DifferentialPilot heisenbot = new DifferentialPilot(56, 153, l_motor, r_motor);
-	static LightSensor l_eye = new LightSensor(SensorPort.S1);
-	static LightSensor r_eye = new LightSensor(SensorPort.S2);
-	
+
+	//static RegulatedMotor l_motor = Motor.B;
+    	//static RegulatedMotor r_motor = Motor.C;
+    	//static DifferentialPilot heisenbot = new DifferentialPilot(56, 153, l_motor, r_motor);
+	//static LightSensor l_eye = new LightSensor(SensorPort.S1);
+	//static LightSensor r_eye = new LightSensor(SensorPort.S2);
+
 	public static void main(String[] args) {
-		
+
 		Button.waitForAnyPress();
 		Delay.msDelay(500);
-		
+
 		heisenbot.setTravelSpeed(100);
 		heisenbot.setRotateSpeed(100);
-		
+
 		while(Button.ENTER.isUp()) {
-			
+
 			int l = l_eye.readValue();
 			int r = r_eye.readValue();
-			
+
 			if(l >= l_turnCondition && r >= r_turnCondition) { //line between eyes
 				if(juncCheck()) {
 					both();
@@ -36,12 +36,12 @@ public class LineFollow extends RobotConfig {
 					straight();
 				}
 			} 
-			
+
 			if(l_eye.readValue() < l_turnCondition && r_eye.readValue() < r_turnCondition) { //line under both eyes 
 				both();
 				route++;
 			} 
-			
+
 			if(l <= l_turnCondition && r >= r_turnCondition) { //line under left eye
 				heisenbot.travel(10);
 				if(juncCheck()) {
@@ -52,7 +52,7 @@ public class LineFollow extends RobotConfig {
 				}
 				System.out.println("turn successful");
 			} 
-			
+
 			if(l >= l_turnCondition && r <= r_turnCondition) { //line under right eye
 				heisenbot.travel(10);
 				if(juncCheck()) {
@@ -66,5 +66,4 @@ public class LineFollow extends RobotConfig {
 		}
 	}
 }
-
 
