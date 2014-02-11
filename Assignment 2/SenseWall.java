9ca13a5d1803c7be09315b66d75446c99a504e3a
Aezure <retep.ster@yahoo.co.uk>
2014-02-11 13:12:18 +0000

    Update SenseWall.java
    MOAR comments.

--- Assignment 2/SenseWall.java
+++ Assignment 2/SenseWall.java
@@ -16,14 +16,17 @@ public class SenseWall extends RobotConfig {
 		while (running) {
 			int distance = sensy.getDistance();
 			int targetDistance = 300;
-		
+
 			// if the targetDistance has been reached, reverse at a speed proportional to distance away
+			// when the actual distance is smaller than target distance (IE too close to target) distance - targetDistance will be negative.
 			if((distance - targetDistance) <= 0)
 			{
 				heisenbot.setTravelSpeed(distance-targetDistance);
 				heisenbot.travel(-10, true);
 			}
 			// if the targetDistance has not been reached, go forward at a speed proportional to distance away
+			// when the actual distance is greater than the target distance (IE too far away) distance - targetDistance will be positive.
+			//this will be the case whenever the first condition is not true, therefore else rather than elseif is used.
 			else 
 			{
 				heisenbot.setTravelSpeed(distance-targetDistance);
@@ -33,7 +36,7 @@ public class SenseWall extends RobotConfig {
 		}
 
 	}
-
+	//the main method ensures that the program will loop from the initial button press until ending button press.
 	public static void main(String[] args) {
 		Button.waitForAnyPress();
 		SenseWall go = new SenseWall();
