b383aad399d04757d638e19e11e92c430b46580c
klanec <nicholas.ss.harris@gmail.com>
2014-03-03 16:31:51 +0000

    Update Node.java

--- Assignment 3/Search/Node.java
+++ Assignment 3/Search/Node.java
@@ -1,7 +1,45 @@
 package search;
 
-public class Node  {
+import java.util.ArrayList;
+
+public class Node<Action, State>  {
+	private Action _action;
+	private State _state;
+	private Node<Action, State> _prevNode; 
 	
+	public static void main(String[] args) {
+		
+	}
 	
+	public Node(State _state) {
+		this(null, _state, null);
+	}
+
+	public Node(Action _action, State _state, Node<Action, State> _prevNode) {
+		this._action = _action;
+		this._state = _state;
+		this._prevNode = _prevNode;
+	}
+
+	public Action getAction() {
+		return _action;
+	}
+
+	public State getState() {
+		return _state;
+	}
+
+	public Node<Action, State> getPrevious() {
+		return _prevNode;
+	}
+	
+	public ArrayList<Action> getRoute() {
+		if(_prevNode != null) {
+			ArrayList<Action> route = _prevNode.getRoute();
+			route.add(getAction());
+			return route;
+		}
+		return new ArrayList<Action>();
+	}
 
 }
