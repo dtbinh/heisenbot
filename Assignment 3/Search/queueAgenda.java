3c8870d40224cf17193a056bca1403712533791e
klanec <nicholas.ss.harris@gmail.com>
2014-03-03 15:17:52 +0000

    queueAgenda.java added
    The Agenda for BFS

---
+++ Assignment 3/Search/queueAgenda.java
@@ -0,0 +1,39 @@
+package search;
+
+import java.util.Iterator;
+
+import rp13.search.interfaces.Agenda;
+
+public class queueAgenda<Node> implements Agenda<> {  //BFS
+
+	@Override
+	public Iterator iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void push(Object _item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean contains(Object _item) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
